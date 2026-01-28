96ed13a1b7362cad594b8d311268666b336f876d
StdBots <stdbots@gmail.com>
2026-01-28 20:22:09 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+from core.logger import setup_logger
+
+setup_logger()
+
+app = Client("StdVoteBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+import handlers.start
+import handlers.vote
+import handlers.callbacks
+import handlers.stats
+import handlers.cancel
+import handlers.admin
+
+print("StdVoteBot Running 🔥")
+app.run()
