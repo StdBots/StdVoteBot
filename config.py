44cbdb18c885f47971daed1a0cc23a0ee8fbd910
StdBots <stdbots@gmail.com>
2026-01-28 20:21:05 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = int(os.getenv("API_ID", 123456))
+API_HASH = os.getenv("API_HASH", "API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "BOT_TOKEN")
+
+ADMIN_ID = 7394590844
+UPDATE_CHANNEL = "StdBots"
+PHOTO_URL = "https://graph.org/file/00ea4effe5d2dfbb8d5be.jpg"
